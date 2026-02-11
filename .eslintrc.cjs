4a53c71f1206217a8673bc10504fcd2aa7153fc5
nirholas <22895867+nirholas@users.noreply.github.com>
2026-02-11 10:51:08 +0000

    🔧 Update eslint and release config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,5 @@
-const config = require('eslint').eslint;
-
 module.exports = {
-  ...config,
   rules: {
-    ...config.rules,
     'sort-keys-fix/sort-keys-fix': 'off',
   },
 };
--- .releaserc.cjs
+++ .releaserc.cjs
@@ -1,3 +1,11 @@
-module.exports = require('eslint').semanticRelease;
+module.exports = {
+    branches: ['main'],
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/npm',
+        '@semantic-release/github',
+    ],
+};
 
 
